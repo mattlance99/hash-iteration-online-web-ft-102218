5b151b337227a348af241e69ee90322dca49afe3
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-23 15:29:27 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    
+  birthday_kids.each do |kids_name, age|
+   puts  "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 
 
